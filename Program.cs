aa0fe692658778925ad12067d362a0e45f7f2492
Roman Šrámek <RomanSramek@gmail.com>
2023-06-09 13:42:59 +0200

    Doplnění zpráv a přidány hodnoty šachovnice

--- Program.cs
+++ Program.cs
@@ -7,9 +7,17 @@ string BV1, BJ1, BS1,  BD,  BK, BS2, BJ2 ,BV2;
 string Èp1, Èp2, Èp3, Èp4, Èp5, Èp6, Èp7, Èp8;
 string ÈV1, ÈJ1, ÈS1, ÈD, ÈK, ÈS2, ÈJ2, ÈV2;
 
+// Pùvodní pozice fugurky
+String PùvodníPozice;
 
 
-// Vytvoøení promìnných pro Šachové figurky BP1 je BílíPìšák1 atd...
+
+// Vytvoøení promìnných pro šachové figurky BP1. B je oznaèení barvy. P je zkratka pìšáka a 1 je jeho èíslo.
+// BV1 je tedy bíla vìž
+// BJ1 bílí jezdec
+// BD bílá dáma
+// BK bílí král
+
 Bp1 = "A2"; Èp1 = "A7";
 Bp2 = "B2"; Èp2 = "B7";
 Bp3 = "C2"; Èp3 = "C7";
@@ -35,7 +43,16 @@ app.MapGet("/Bp1", () =>
 
 app.MapGet("/Bp1/{zmenaBp1}", (string zmenaBp1) =>
 {
-    Bp1 = zmenaBp1;
+    PùvodníPozice = Bp1;
+
+    if (zmenaBp1.Length == 2 && zmenaBp1[0] >= 'A' && zmenaBp1[0] <= 'H' && zmenaBp1[1] >= '1' && zmenaBp1[1] <= '8')
+    {
+        Bp1 = zmenaBp1;
+    }
+    else
+    {
+        return "neplatné souøadnice (maximální hodnoty jsou 1-8 a A-H.";
+    }
 
     Dictionary<string, string> promenne = new Dictionary<string, string>();
     // Pøidání promìnných do slovníku
@@ -50,11 +67,12 @@ app.MapGet("/Bp1/{zmenaBp1}", (string zmenaBp1) =>
     promenne.Add("BV1", BV1);
     promenne.Add("BJ1", BJ1);
     promenne.Add("BS1", BS1);
-    promenne.Add("BD", BD);
-    promenne.Add("BK", BK);
+    promenne.Add("BD"  , BD);
+    promenne.Add("BK"  , BK);
     promenne.Add("BS2", BS2);
     promenne.Add("BJ2", BJ2);
     promenne.Add("BV2", BV2);
+    // další pokraèování by mìlo být nejspíše do budoucna rozdìleno kvùli pravidlùm ve høe.
     promenne.Add("Èp1", Èp1);
     promenne.Add("Èp2", Èp2);
     promenne.Add("Èp3", Èp3);
@@ -66,30 +84,35 @@ app.MapGet("/Bp1/{zmenaBp1}", (string zmenaBp1) =>
     promenne.Add("ÈV1", ÈV1);
     promenne.Add("ÈJ1", ÈJ1);
     promenne.Add("ÈS1", ÈS1);
-    promenne.Add("ÈD", ÈD);
-    promenne.Add("ÈK", ÈK);
+    promenne.Add("ÈD"  , ÈD);
+    promenne.Add("ÈK"  , ÈK);
     promenne.Add("ÈS2", ÈS2);
     promenne.Add("ÈJ2", ÈJ2);
     promenne.Add("ÈV2", ÈV2);
 
-    // Pøidej další promìnné
+    
 
     // Procházení slovníku
     foreach (var kvp in promenne)
     {
         string nazevPromenne = kvp.Key;
         string hodnotaPromenne = kvp.Value;
+        string nazevPromenneSeStejnouHodnotou = "";
 
         // Kontrola, zda jiná promìnná již má stejnou hodnotu
         bool hodnotaSeOpakuje = promenne.Any(p => p.Key != nazevPromenne && p.Value == hodnotaPromenne);
 
         if (hodnotaSeOpakuje)
         {
-            Console.WriteLine("Není možné nastavit hodnotu promìnné " + nazevPromenne + ", protože již existuje jiná promìnná s touto hodnotou.");
+            // uložení názvu promìnné která má již stejné pole do nazevPromenneSeStejnouHodnotou
+            nazevPromenneSeStejnouHodnotou = promenne.FirstOrDefault(p => p.Key != nazevPromenne && p.Value == hodnotaPromenne).Key;
+            Bp1 = PùvodníPozice;
+            return ("Není možné táhnout s  "+nazevPromenne+ " na " +zmenaBp1+ " , na tomto poli je již " +nazevPromenneSeStejnouHodnotou);
         }
         else
         {
-            Console.WriteLine("Hodnota promìnné " + nazevPromenne + " je unikátní.");
+
+            return (nazevPromenne +" se nyní nachází poli " + Bp1);
         }
     }
 
